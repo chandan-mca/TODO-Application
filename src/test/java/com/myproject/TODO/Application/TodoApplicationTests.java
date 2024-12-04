93fd62a294b1b8ace3a68837c82bb730b2f09c41
Chandan Agrawal <cagrawal0012@gmail.com>
2024-12-04 17:25:13 +0530

    first commit

---
+++ src/test/java/com/myproject/TODO/Application/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.TODO.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
